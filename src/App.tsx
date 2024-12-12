865fbeb9ff0cf5eb464dc6f94a95488b9dba9ccb
sowmik-sec <sowmik.sec@gmail.com>
2024-12-12 09:33:56 +0600

    Initialize store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <>
-      <h2 className="text-3xl">react with redux</h2>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border-purple-300 rounded-md bg-slate-50 p-10">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">
+          Decrement
+        </button>
+      </div>
+    </div>
   );
 }
 
