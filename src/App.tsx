de78c162839edab64d33c67c7606e0e346731e80
sowmik-sec <sowmik.sec@gmail.com>
2024-12-12 11:38:24 +0600

    delete redux part to begin fresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import {
-  decrement,
-  increment,
-  incrementByValue,
-} from "./redux/features/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
-
 function App() {
-  const { count } = useAppSelector((state) => state.counter);
-  const dispatch = useAppDispatch();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border-purple-300 rounded-md bg-slate-50 p-10">
-        <button
-          onClick={() => dispatch(increment())}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
-        >
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
           Increment
         </button>
-        <button
-          onClick={() => dispatch(incrementByValue(5))}
-          className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
-        >
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
           Increment by 5
         </button>
-        <h1 className="text-3xl mx-10">{count}</h1>
-        <button
-          onClick={() => dispatch(decrement())}
-          className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white"
-        >
+        <h1 className="text-3xl mx-10">{0}</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">
           Decrement
         </button>
       </div>
--- src/redux/middleware/logger.ts
+++
@@ -1,8 +0,0 @@
-const Logger = (state) => (next) => (action) => {
-  console.log("Current state =>", state.getState());
-  console.log("Current state =>", action);
-  next(action);
-  console.log("Current state =>", action);
-};
-
-export default Logger;
